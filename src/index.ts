cb2901c44987d81b96aa3e0dd6b60b68ea155ebb
robin jacobsson <robin.jacobsson@kebne.se>
2023-11-18 12:13:45 +0100

    Restructure a little

--- src/index.ts
+++ src/index.ts
@@ -12,31 +12,29 @@ const app = new Application({
 
 const stage = app.stage;
 
-window.onload = async (): Promise<void> => {
-  const textures = await loadGameAssets();
-
-  document.body.appendChild<HTMLCanvasElement>(app.view as HTMLCanvasElement);
-
-  resizeCanvas();
-
-  const backgroundSprite = Sprite.from(textures.background);
-  stage.addChild(backgroundSprite);
-};
 
 async function loadGameAssets() {
   Assets.add('background', 'assets/Background.png');
   Assets.add('button', 'assets/button.png');
+  
   return Assets.load(['background', 'button']);
 }
 
 function resizeCanvas(): void {
-  const resize = () => {
-    app.renderer.resize(window.innerWidth, window.innerHeight);
-    app.stage.scale.x = window.innerWidth / gameWidth;
-    app.stage.scale.y = window.innerHeight / gameHeight;
-  };
-
-  resize();
-
-  window.addEventListener('resize', resize);
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+  stage.scale.x = window.innerWidth / gameWidth;
+  stage.scale.y = window.innerHeight / gameHeight; 
 }
+
+
+window.onload = async (): Promise<void> => {
+  const textures = await loadGameAssets();
+  const backgroundSprite = Sprite.from(textures.background);
+
+  document.body.appendChild<HTMLCanvasElement>(app.view as HTMLCanvasElement);
+  resizeCanvas();
+
+  stage.addChild(backgroundSprite);
+};
+
+window.addEventListener('resize', resizeCanvas);
