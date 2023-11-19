32622c36c5185bb98a29fb94718b20099fdad37e
robin jacobsson <robin.jacobsson@kebne.se>
2023-11-19 17:20:55 +0100

    Enable more then one number to be selected

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import { Assets, Sprite, Text, SpriteSource } from "pixi.js";
 import { createButton, Button } from "./button";
 import {
   game,
-  stage,
   Tuple,
   takeFirst,
   useScale,
   setPosition,
   repeat9,
+  Selected,
 } from "./shared";
 
 Text.defaultResolution = 2;
@@ -23,9 +23,9 @@ const otherTextures: Tuple<string>[] = [
   ["win", "assets/win.png"],
 ];
 
-const symTextures: Tuple<string>[] = repeat9((index) => [
-  `sym${index + 1}`,
-  `assets/sym${index + 1}.png`,
+const symTextures: Tuple<string>[] = repeat9((num) => [
+  `sym${num}`,
+  `assets/sym${num}.png`,
 ]);
 
 async function loadGameAssets(): Promise<Record<string, SpriteSource>> {
@@ -39,7 +39,7 @@ async function loadGameAssets(): Promise<Record<string, SpriteSource>> {
 }
 
 function resizeCanvas(): void {
-  game.renderer.resize(window.innerWidth, window.innerHeight);
+  game.app.renderer.resize(window.innerWidth, window.innerHeight);
   //stage.scale.x = window.innerWidth / gameWidth;
   //stage.scale.y = window.innerHeight / gameHeight;
 }
@@ -55,10 +55,7 @@ window.onload = async (): Promise<void> => {
   useScale(lose)(1.5);
   setPosition(lose, 0.5, 0.5);
 
-  const chooseButton = createButton(button, 0.5, 0.8, 120, 110, () =>
-    finalGameState(1)
-  );
-
+  const chooseButton = createButton(button, 0.5, 0.8, 120, 110, goToFinal);
 
   const playAgainButton = createButton(
     new Text("PLAY AGAIN", { fill: 0xffffff, fontSize: 14 }),
@@ -66,57 +63,73 @@ window.onload = async (): Promise<void> => {
     0.8,
     150,
     50,
-    initialGameState
+    goToStart
   );
 
-  const selectionButtons: Button[] = repeat9(index => {
-    const [key,] = symTextures[index];
-    const sprite = Sprite.from(textures[key])
+  const selectionButtons: Button[] = repeat9((num) => {
+    const [key] = symTextures[num - 1];
+    const sprite = Sprite.from(textures[key]);
 
-    const button = createButton(sprite, 0.1 * (index + 1), 0.5, 90, 90, () => {
-      button.active = !button.active;
-      chooseButton.source.visible = false;
-    })
+    const button = createButton(sprite, 0.1 * num, 0.5, 90, 90, () => {
+      game.select(num as Selected);
+
+      for (const [index, button] of selectionButtons.entries()) {
+        button.active = game.hasSelected(index + 1 as Selected);
+      }
+
+      chooseButton.source.visible = game.maxSelectionReached;
+    });
 
     return button;
-  })
+  });
 
-  function finalGameState(selection: number) {
-    selectionButtons[selection - 1].source.visible = false;
+  function goToFinal() {
+    for (const button of selectionButtons) {
+      button.source.visible = false;
+      button.active = false;
+    }
+
     chooseButton.source.visible = false;
     playAgainButton.source.visible = true;
 
-    (Math.floor(Math.random() * 9) + 1 === selection ? win : lose).visible =
-      true;
+    (game.hasSelected((Math.floor(Math.random() * 9) + 1) as Selected)
+      ? win
+      : lose
+    ).visible = true;
   }
 
-  function initialGameState() {
+  function goToStart() {
     win.visible = false;
     lose.visible = false;
 
-    for(const button of selectionButtons) {
+    for (const button of selectionButtons) {
       button.source.visible = true;
       button.active = false;
     }
 
     playAgainButton.source.visible = false;
+    chooseButton.source.visible = false;
+
+    game.clearSelection();
   }
 
-  document.body.appendChild<HTMLCanvasElement>(game.view as HTMLCanvasElement);
+  document.body.appendChild<HTMLCanvasElement>(
+    game.app.view as HTMLCanvasElement
+  );
 
   resizeCanvas();
-  initialGameState();
+  goToStart();
 
-  stage.addChild(background);
-  stage.addChild(chooseButton.source);
+  game.stage.addChild(background);
+  game.stage.addChild(chooseButton.source);
 
-  for(const button of selectionButtons) {
-    stage.addChild(button.source);
+  for (const button of selectionButtons) {
+    game.stage.addChild(button.source);
   }
 
-  stage.addChild(lose);
-  stage.addChild(win);
-  stage.addChild(playAgainButton.source);
+  game.stage.addChild(lose);
+  game.stage.addChild(win);
+  game.stage.addChild(playAgainButton.source);
 };
 
 window.addEventListener("resize", resizeCanvas);
--- src/shared.ts
+++ src/shared.ts
@@ -22,32 +22,52 @@ const useScale =
     source.height = (height ?? source.height) * scale;
   };
 
-
 const gameWidth = 1136;
 const gameHeight = 640;
 
-const game = new Application({
-  backgroundColor: 0xd3d3d3,
+const app = new Application({
+  backgroundColor: 0x00000,
   width: gameWidth,
   height: gameHeight,
 });
 
-const stage = game.stage;
+type Selected = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+let selectedValues: Set<Selected> = new Set();
+
+const game = {
+  app,
+  stage: app.stage,
+  select: (value: Selected) => {
+    if (selectedValues.has(value)) {
+      selectedValues.delete(value);
+    } else if (selectedValues.size < 5) {
+      selectedValues.add(value);
+    }
+  },
+  hasSelected: (value: Selected) => selectedValues.has(value),
+  get maxSelectionReached() {
+    return selectedValues.size === 5;
+  },
+  clearSelection: () => selectedValues.clear()
+};
 
 /** Sets the position with normalized coordinates (values 0 - 1) within the game screen */
 const setPosition = (source: Sprite | Text, x: number, y: number) => {
-    source.anchor.set(0.5);
-    source.x = source.width / 2 + (gameWidth - source.width) * x;
-    source.y = source.height / 2 + (gameHeight - source.height) * y;
-}
+  source.anchor.set(0.5);
+  source.x = source.width / 2 + (gameWidth - source.width) * x;
+  source.y = source.height / 2 + (gameHeight - source.height) * y;
+};
 
-const repeat9 = <T>(fn: (index: number) => T): T[] => Array.from({ length: 9 }, (_, index) => fn(index));
+const repeat9 = <T>(fn: (index: number) => T): T[] =>
+  Array.from({ length: 9 }, (_, index) => fn(index + 1));
 
 export {
   Tuple,
   Point,
   Sprite,
   Text,
+  Selected,
   takeFirst,
   takeSecond,
   toPoint,
@@ -56,7 +76,6 @@ export {
   setPosition,
   repeat9,
   game,
-  stage,
   gameWidth,
-  gameHeight
+  gameHeight,
 };
